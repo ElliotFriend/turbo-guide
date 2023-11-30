72aa350ee7a84d19d9c3ff5d277309b6be6bf922
Elliot Voris <elliot@stellar.org>
2023-11-30 17:04:13 -0600

    more boilerplate

--- 01/main.py
+++ 01/main.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+import os
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__))
+)
+
+SAMPLE_DATA = True
+
 contents = None
-with open('input.txt', 'r') as f:
-    contents = f.read().split('\n\n')
+with open(os.path.join(__location__, f'{"sample" if SAMPLE_DATA else "input"}.txt'), 'r') as f:
+    contents = f.read().splitlines()
--- 27/main.py
+++ 27/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+import os
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__))
+)
+
+SAMPLE_DATA = True
+
+contents = None
+with open(os.path.join(__location__, f'{"sample" if SAMPLE_DATA else "input"}.txt'), 'r') as f:
+    contents = f.read().splitlines()
