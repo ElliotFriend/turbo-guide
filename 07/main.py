76d955fbe8ac63a0d103ab1233008239e3df27ff
Elliot Voris <elliot@stellar.org>
2023-12-01 11:26:54 -0600

    black formatting the boilerplate

--- 07/main.py
+++ 07/main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import os
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__))
-)
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 SAMPLE_DATA = True
 
 contents = None
-with open(os.path.join(__location__, f'{"sample" if SAMPLE_DATA else "input"}.txt'), 'r') as f:
+with open(
+    os.path.join(__location__, f'{"sample" if SAMPLE_DATA else "input"}.txt'), "r"
+) as f:
     contents = f.read().splitlines()
