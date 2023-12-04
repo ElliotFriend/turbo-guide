68d9241e82ab128302404791ab8ca49a8b6331af
Elliot Voris <elliot@stellar.org>
2023-12-04 17:33:37 -0600

    linting day 3

--- 03/day03.py
+++ 03/day03.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Day 02 of 2023 Advent of Code."""
+"""Day 03 of 2023 Advent of Code."""
 
 import os
 import re
@@ -19,19 +19,23 @@ with open(
     CONTENTS = f.read().splitlines()
 
 
-for i in range(len(CONTENTS)):
-    mi1 = re.finditer(r'\d+', CONTENTS[i])
+for i, line in enumerate(CONTENTS):
+    mi1 = re.finditer(r"\d+", line)
     for m in mi1:
         if m.group:
             start = 0 if m.start() == 0 else m.start() - 1
-            end = len(CONTENTS[i]) if m.end() == len(CONTENTS[i]) else m.end() + 1
-            m1 = [] if i == 0 else re.findall(r'[^\d\.]', CONTENTS[i-1][start:end])
-            m2 = re.findall(r'[^\d\.]', CONTENTS[i][start:end])
-            m3 = [] if i == len(CONTENTS) - 1 else re.findall(r'[^\d\.]', CONTENTS[i+1][start:end])
+            end = len(line) if m.end() == len(line) else m.end() + 1
+            m1 = [] if i == 0 else re.findall(r"[^\d\.]", CONTENTS[i - 1][start:end])
+            m2 = re.findall(r"[^\d\.]", line[start:end])
+            m3 = (
+                []
+                if i == len(CONTENTS) - 1
+                else re.findall(r"[^\d\.]", CONTENTS[i + 1][start:end])
+            )
             if m1 or m2 or m3:
                 PART_NUMBERS_SUM += int(m.group())
 
-    mi2 = re.finditer(r'\*', CONTENTS[i])
+    mi2 = re.finditer(r"\*", line)
     for m in mi2:
         if m.group:
             adjacent_parts = []
@@ -39,22 +43,22 @@ for i in range(len(CONTENTS)):
 
             # check the previous line
             if i > 0:
-                m1 = re.finditer(r'\d+', CONTENTS[i - 1])
+                m1 = re.finditer(r"\d+", CONTENTS[i - 1])
                 for m11 in m1:
                     if coords[0] >= m11.start() - 1 and coords[0] <= m11.end():
                         adjacent_parts.append(int(m11.group()))
 
             # check same line
-            m2a = re.search(r'\d+$', CONTENTS[i][:coords[0]])
+            m2a = re.search(r"\d+$", line[: coords[0]])
             if m2a:
                 adjacent_parts.append(int(m2a.group()))
-            m2b = re.search(r'^\d+', CONTENTS[i][coords[0]+1:])
+            m2b = re.search(r"^\d+", line[coords[0] + 1 :])
             if m2b:
                 adjacent_parts.append(int(m2b.group()))
 
             # check the next line
             if i < len(CONTENTS):
-                m3 = re.finditer(r'\d+', CONTENTS[i + 1])
+                m3 = re.finditer(r"\d+", CONTENTS[i + 1])
                 for m33 in m3:
                     if coords[0] >= m33.start() - 1 and coords[0] <= m33.end():
                         adjacent_parts.append(int(m33.group()))
