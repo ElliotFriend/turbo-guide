27765b739fac1593dad01caee9551f0be6d2057a
Elliot Voris <elliot@stellar.org>
2023-12-04 17:31:25 -0600

    day 4 done and dusted!

--- 04/day04.py
+++ 04/day04.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
-"""Day 02 of 2023 Advent of Code."""
+"""Day 04 of 2023 Advent of Code.
+"""
 
 import os
+import re
+
+from dataclasses import dataclass, field
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-SAMPLE_DATA: bool = True
+SAMPLE_DATA: bool = False  # part 1: 13, part 2: 30
 CONTENTS: list[str] = []
+TOTAL_SCRATCHCARDS: int = 0
 
 with open(
     os.path.join(__location__, f'{"sample" if SAMPLE_DATA else "input"}.txt'),
@@ -14,3 +19,71 @@ with open(
     encoding="utf-8",
 ) as f:
     CONTENTS = f.read().splitlines()
+
+
+@dataclass
+class Scratchcard:
+    """A single scratchcard that contains a list of winning numbers and list of
+    "numbers you have."
+    """
+
+    id: int = 0
+    winning_numbers: list[int] = field(default_factory=lambda: [])
+    numbers_you_have: list[int] = field(default_factory=lambda: [])
+    original_card: int = 1
+    duplicate_cards: int = 0
+
+    def points_value(self) -> int:
+        """Returns the total point value of this scratchcard (part 1)
+
+        Returns:
+            int: the total value of all matches on this card
+        """
+        counter: int = 0
+        for n in self.numbers_you_have:
+            if n in self.winning_numbers:
+                counter = 1 if counter == 0 else counter * 2
+        return counter
+
+    def matching_numbers(self) -> int:
+        """Returns how many matching numbers this scratchcard has.
+
+        Returns:
+            int: how many matching "numbers you have" this card contains
+        """
+        return len([n for n in self.numbers_you_have if n in self.winning_numbers])
+
+    def total_cards(self) -> int:
+        """Returns the total cards (original + duplicate) instances we have.
+
+        Returns:
+            int: the total number of this card we have
+        """
+        return self.original_card + self.duplicate_cards
+
+    def add_duplicates(self, quant: int = 1) -> None:
+        """Adds a duplicate card of this instance (i.e., we won another copy)."""
+        self.duplicate_cards += quant
+
+
+cards: list[Scratchcard] = []
+
+for i, card in enumerate(CONTENTS):
+    [card_details, card_numbers] = card.split(":")
+    [winners, possessed] = card_numbers.split("|")
+    cards.append(
+        Scratchcard(
+            id=re.search(r"\d+$", card_details).group(),
+            winning_numbers=[int(n) for n in winners.strip().split(" ") if n],
+            numbers_you_have=[int(n) for n in possessed.strip().split(" ") if n],
+        )
+    )
+
+
+for i, card in enumerate(cards):
+    TOTAL_SCRATCHCARDS += card.total_cards()
+    for j in range(1, card.matching_numbers() + 1):
+        cards[i + j].add_duplicates(card.total_cards())
+
+print(f"Part 1: {sum(c.points_value() for c in cards)}")
+print(f"Part 2: {TOTAL_SCRATCHCARDS}")
