103a791640d9cce7af522840b2267f3011e85720
Elliot Voris <elliot@stellar.org>
2023-12-06 01:22:22 -0600

    linting and type checking day 6

--- 06/day06.py
+++ 06/day06.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, field
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-SAMPLE_DATA: bool = True  # part 1: 288, part 2: 71503
+SAMPLE_DATA: bool = False  # part 1: 288, part 2: 71503
 CONTENTS: list[str] = []
 WINNING_STRATS_PRODUCT: int = 1
 
@@ -17,42 +17,55 @@ with open(
 ) as f:
     CONTENTS = f.read().splitlines()
 
-@dataclass
-class RaceStrategy:
-    race_time: int = 0
-    charge_time: int = 0
-    distance_traveled: int = 0
-
-    def simulate(self) -> int:
-        """Simulates how the race goes following this strategy.
-
-        Returns:
-            int: the distance traveled by the boat during the race
-        """
-        speed = self.charge_time
-        remaining_time = self.race_time - self.charge_time
-        self.distance_traveled = speed * remaining_time
-        return self.distance_traveled
-
 
 @dataclass
 class BoatRace:
+    """A single boat race from the sheet of race records given to you by an elf."""
+
     duration: int = 0
     best_distance: int = 0
-    winning_strategies: list[RaceStrategy] = field(default_factory=lambda: [])
 
-    def run_simulations(self):
-        for i in range(self.duration + 1):
-            strat = RaceStrategy(race_time=self.duration, charge_time=i)
-            if strat.simulate() > self.best_distance:
-                self.winning_strategies.append(strat)
+    def calculate_button_press(self, min_bound: bool = True, iterator: int = 1) -> int:
+        """Calculate and return the longest or shortest amount of time the boat
+        button could be pressed and still beat the record.
+
+        Args:
+            min_bound (boolean, optional): True if clalculating minimum
+                boundary, False for maximum boundary. Defaults to True.
+            iterator (int, optional): Whether to increase or decrease the
+                terator. Defaults to 1.
+
+        Returns:
+            int: the relevant min/max time the boat button could be pressed
+        """
+        button_press: int = 1 if min_bound else self.duration
+        while True:
+            dist_traveled = (self.duration - button_press) * button_press
+            if dist_traveled > self.best_distance:
+                if min_bound:
+                    return button_press
+                return button_press + 1
+            button_press += iterator
 
     def count_winning_strategies(self) -> int:
-        return len(self.winning_strategies)
+        """Calculates and returns the number of strategies that could beat the
+        all-time record in this boat race.
+
+        Returns:
+            int: The number of ways you could beat the record in this race.
+        """
+        return (
+            self.calculate_button_press(min_bound=False, iterator=-1)
+            - self.calculate_button_press()
+        )
+
 
 @dataclass
 class RaceSheet:
+    """A sheet containing one or more race records."""
+
     races: list[BoatRace] = field(default_factory=lambda: [])
+
 
 race_details = zip(
     map(int, CONTENTS[0].split()[1:]),
@@ -60,21 +73,23 @@ race_details = zip(
 )
 
 big_race = (
-    int(''.join(CONTENTS[0].split()[1:])),
-    int(''.join(CONTENTS[1].split()[1:])),
+    int("".join(CONTENTS[0].split()[1:])),
+    int("".join(CONTENTS[1].split()[1:])),
 )
-print(big_race)
 
-part1_race_sheet = RaceSheet([BoatRace(duration=t, best_distance=d) for t, d in race_details])
+part1_race_sheet = RaceSheet(
+    [BoatRace(duration=t, best_distance=d) for t, d in race_details]
+)
 for r in part1_race_sheet.races:
-    r.run_simulations()
+    # r.run_simulations()
     WINNING_STRATS_PRODUCT *= r.count_winning_strategies()
 
 print(f"Part 1: {WINNING_STRATS_PRODUCT}")
 # 2756160
 
 
-part2_race_sheet = RaceSheet([BoatRace(duration=big_race[0], best_distance=big_race[1])])
-part2_race_sheet.races[0].run_simulations()
+part2_race_sheet = RaceSheet(
+    [BoatRace(duration=big_race[0], best_distance=big_race[1])]
+)
 print(f"Part 2: {part2_race_sheet.races[0].count_winning_strategies()}")
 # 34788142
