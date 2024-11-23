9c95db8923c603e7ff296f52af22e14dab180044
Elliot Voris <elliot@stellar.org>
2024-11-22 20:01:04 -0600

    old stuff from last year?

--- 08/day08.py
+++ 08/day08.py
@@ -1,13 +1,15 @@
 """Day 08 of 2023 Advent of Code."""
 
+import math
 import os
 
 from dataclasses import dataclass
 from itertools import cycle
+from multiprocessing import Pool
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-SAMPLE_DATA: bool = False  # part 1: 2?, part 2: something
+SAMPLE_DATA: bool = False  # part 1: 2, part 2: 6
 CONTENTS: list[str] = []
 START_NODE: str = "AAA"
 END_NODE: str = "ZZZ"
@@ -20,27 +22,46 @@ with open(
     CONTENTS = f.read().splitlines()
 
 left_right_steps = cycle(list(CONTENTS[0]))
-# print(next(left_right_steps))
 
-# @dataclass
-# class NetworkNodeMap:
-
-map_dict = dict()
+map_dict: dict[str, dict[str, str]] = dict()
 
 for node_map in list(CONTENTS[2:]):
     # print(node_map.split())
     node_name, _, left_node, right_node = node_map.split()
     # print(node_name)
-    map_dict[node_name] = {'L': left_node.replace("(", "").replace(",", ""), "R": right_node.replace(")", "")}
+    map_dict[node_name] = {
+        "L": left_node.replace("(", "").replace(",", ""),
+        "R": right_node.replace(")", ""),
+    }
 
-# print(map_dict)
+
+def take_step(current_node: str, direction: str) -> str:
+    return map_dict[current_node][direction]
+
 
 current_node = START_NODE
 step_count: int = 0
 
 while current_node != END_NODE:
-    current_node = map_dict[current_node][next(left_right_steps)]
+    current_node = take_step(current_node, next(left_right_steps))
     step_count += 1
 
 print(f"Part 1: {step_count}")
 # 11911
+
+step_counts: list[int] = []
+
+for node in list(n for n, _ in map_dict.items() if n.endswith("A")):
+    current_node = node
+    step_count = 0
+    while not current_node.endswith("Z"):
+        step_count += 1
+        current_node = take_step(current_node, next(left_right_steps))
+    step_counts.append(step_count)
+
+lcm = 1
+for count in step_counts:
+    lcm *= count // math.gcd(lcm, count)
+
+print(f"Part 2: {lcm}")
+# 10151663816849
--- 11/day11.py
+++ 11/day11.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass, field
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-SAMPLE_DATA: bool = False  # part 1: 374, part 2: something
+SAMPLE_DATA: bool = True  # part 1: 374, part 2: 1030 and then 8410
 CONTENTS: list[str] = []
 
 with open(
@@ -31,6 +31,7 @@ class Galaxy:
 @dataclass
 class GalaxyImage:
     og_image: list[str] = field(default_factory=lambda: [])
+    exp_val: int = 1
     expanded_image: list[str] = field(default_factory=lambda: [])
     galaxies: list[Galaxy] = field(default_factory=lambda: [])
 
@@ -39,7 +40,8 @@ class GalaxyImage:
         for _, row in enumerate(self.og_image):
             self.expanded_image.append(row)
             if not '#' in row:
-                self.expanded_image.append(row)
+                for i in range(self.exp_val - 1 or 1):
+                    self.expanded_image.append(row)
 
         # then do the cols
         empty_cols: list[int] = []
@@ -70,21 +72,30 @@ class GalaxyImage:
 
     def insert_empty_col(self, index: int) -> None:
         for i, row in enumerate(self.expanded_image):
-            self.expanded_image[i] = f"{row[:index]}.{row[index:]}"
+            self.expanded_image[i] = f"{row[:index]}{'.' * (self.exp_val - 1 or 1)}{row[index:]}"
 
     def __post_init__(self) -> None:
         self.expand_universe()
         self.discover_galaxies()
 
 
-gi = GalaxyImage(CONTENTS)
+gi1 = GalaxyImage(CONTENTS)
 total_sums: int = 0
 
-for g in gi.galaxies:
-    for h in gi.galaxies:
+for g in gi1.galaxies:
+    for h in gi1.galaxies:
         m_dist = g.manhattan_dist(h.coords)
         total_sums += m_dist
         # print(f"g={g.coords} h={h.coords} m_dist={m_dist}")
 # print(sum([g.manhattan_dist((h.coords[0], h.coords[1]) for h in gi.galaxies) for g in gi.galaxies]))
 print(f"Part 1: {total_sums // 2}")
 # 9957702
+
+gi2 = GalaxyImage(CONTENTS, 100)
+total_sums = 0
+
+for g in gi2.galaxies:
+    for h in gi2.galaxies:
+        m_dist = g.manhattan_dist(h.coords)
+        total_sums += m_dist
+print(f"Part 2: {total_sums // 2}")
