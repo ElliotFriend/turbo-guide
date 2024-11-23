65e657281e2015f29a7e41e41f14fcf133bb4caa
Elliot Voris <elliot@stellar.org>
2024-11-22 21:02:53 -0600

    boilerplate for this year

---
+++ 2024/11/day11.py
@@ -0,0 +1,15 @@
+"""Day 11 of 2024 Advent of Code."""
+
+import os
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+SAMPLE_DATA: bool = True
+CONTENTS: list[str] = []
+
+with open(
+    os.path.join(__location__, f'{"sample" if SAMPLE_DATA else "input"}.txt'),
+    "r",
+    encoding="utf-8",
+) as f:
+    CONTENTS = f.read().splitlines()
